6eaf8c6221e9b2f4a61e64da1a247d89f097525c
Lynn <32795221+linnnruoo@users.noreply.github.com>
2018-05-07 15:37:54 +0800

    Update review.cpp

--- CG1112/review.cpp
+++ CG1112/review.cpp
@@ -2,37 +2,37 @@
 #include <evilprof.h>
 
 int main(){
-  int week, studio, celc, assignment, presentation;
-  int pain, tears, sleep, social_life, meal_at_McDonalds;
-  bool vincent_is_working = false;
-  
-  pain = tears = meal_at_McDonalds = 0;
-  social_life = sleep = INF;
-  
-  for(int week=1; week<=13; week++){
-    bool whats_going_on = true;
-    bool can_i_see_ur_report = true;
-    
-    meal_at_McDonalds++;
-   
-    if (week > 7){
-      pain++;
-      tears++;
-      sleep--;
-      social_life--;
-      meal_at_McDonalds += 3;
-  
-      if (vincent_is_working) ? pain-- : pain++;
-    }
-    
-    if(week == 13) {
-       Murphys_Law_effect = true;
-    }
-    
-  }
-  
+    int week, studio, celc, assignment, presentation;
+    int pain, tears, sleep, social_life, meal_at_McDonalds;
+    bool vincent_is_working = false;
 
-  return vincent;
+    pain = tears = meal_at_McDonalds = 0;
+    social_life = sleep = INF;
+
+    for(int week=1; week<=13; week++){
+      bool whats_going_on = true;
+      bool can_i_see_ur_report = true;
+
+      meal_at_McDonalds++;
+
+      if (week > 7){
+        pain++;
+        tears++;
+        sleep--;
+        social_life--;
+        meal_at_McDonalds += 3;
+
+        if (vincent_is_working) ? pain-- : pain++;
+      }
+
+      if(week == 13) {
+         Murphys_Law_effect = true;
+      }
+
+    }
+
+
+    return vincent;
 }
 
 //this code is definitely not executable
